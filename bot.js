5f82f4aabe2d702f1fdaaf899a0779090b2e7a07
FKA <soffersfka@gmail.com>
2019-04-20 16:56:40 +0200

    testing username mention

--- bot.js
+++ bot.js
@@ -46,8 +46,15 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             case 'test':
                 // grab the "first" mentioned user from the message
                 // this will return a `User` object, just like `message.author`
+
                 const taggedUser = message.mentions.users.first();
-                message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+                //message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+                bot.sendMessage(
+                    {
+                        to: channelID,
+                        message: "You wanted to kick:" + taggedUser.username
+                    }
+                );
 
                 break;
         }
