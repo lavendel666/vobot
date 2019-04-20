e1c6fe70c0f2efd285fe5bcb5753b1a8732b8a5b
FKA <soffersfka@gmail.com>
2019-04-20 18:24:03 +0200

    added gitignore

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const {prefix, token} = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log('Ready')
+})
+
+// client.on('message', message => {
+//     // logt elk getypt bericht in Discord naar de terminal in PHPStorm
+//     console.log(message.content);
+// })
+
+client.on('message', message => {
+    if(message.content.startsWith(`${prefix}test`)){
+        message.channel.send("TESTEST")
+    }
 })
 
 client.login(token);
