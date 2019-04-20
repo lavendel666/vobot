a1b750e8bb4d9652cda84c7a005f9a06512ea0ef
FKA <soffersfka@gmail.com>
2019-04-20 20:10:19 +0200

    back online, added a gitignore file so VObot won't be hacked again (we hope)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log('Ready')
-})
+});
 
 // client.on('message', message => {
 //     // logt elk getypt bericht in Discord naar de terminal in PHPStorm
@@ -12,9 +12,24 @@ client.once('ready', () => {
 // })
 
 client.on('message', message => {
-    if(message.content.startsWith(`${prefix}kak`)){
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (message.content.startsWith(`${prefix}kak`)) {
         message.channel.send("VO VOOR DE LEDEN")
     }
-})
+    // using the new `command` variable, this makes it easier to manage!
+    // you can switch your other commands to this format as well
+    else if (command === 'args-info') {
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+
+        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+    }
+
+});
 
 client.login(token);
