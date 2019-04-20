43325ef96ccea14e37cf6ff9f12d2d7fe4b4e2cf
FKA <soffersfka@gmail.com>
2019-04-20 16:12:17 +0200

    ad a space to the command

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `vo`
-    if (message.substring(0, 2) == 'vo') {
+    if (message.substring(0, 2) == 'vo ') {
         var args = message.substring(2).split(' ');
         var cmd = args[0];
        
