96f2174ee9e0d25eb3093a7408f0b9eeae22c4ec
FKA <soffersfka@gmail.com>
2019-04-20 15:35:21 +0200

    substring edit, '!'-> 'vo'

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == 'vo') {
+    if (message.substring(0, 2) == 'vo') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
@@ -34,7 +34,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 });
             break;
 
-            // VO 
+            // !VO 
             case 'vo':
             bot.sendMessage({
                 to: channelID,
