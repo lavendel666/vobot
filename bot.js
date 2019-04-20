006130018bd632f2a6d2d9ac192280286e2f158e
FKA <soffersfka@gmail.com>
2019-04-20 17:00:41 +0200

    jhb

--- bot.js
+++ bot.js
@@ -47,7 +47,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 // grab the "first" mentioned user from the message
                 // this will return a `User` object, just like `message.author`
 
-                const taggedUser = message.mentions.users.first();
+                const taggedUser = message.mentions.members.first();
                 //message.channel.send(`You wanted to kick: ${taggedUser.username}`);
                 bot.sendMessage(
                     {
