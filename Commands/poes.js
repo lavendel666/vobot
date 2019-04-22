e7592c51282c8edf62b6c3b3a00667bf401ad152
FKA <soffersfka@gmail.com>
2019-04-22 17:47:55 +0200

    Poes command werkt nu, zlefs met 2 oplossingen.
    Let op dat execute hier nog steeds async is

--- Commands/poes.js
+++ Commands/poes.js
@@ -1,39 +1,20 @@
 module.exports = {
     name: 'poes',
-    description: 'Laat een random poes foto zien',
-    execute(message, args) {
-        // const {body} = await fetch('https://aws.random.cat/meow').then(response => response.json());
-        // message.channel.send(body.file);
-        const fetch = require('node-fetch');
-
-        async function fetchCat() {
-            const {body} = await fetch('https://aws.random.cat/meow')
-                .then(response => {
-                    return response.json();
-                });
-            return body.file;
-        }
-
-        try {
-            var poez = fetchCat();
-        } catch (e) {
-            console.log('kut');
-        }
-
-        message.channel.send(poez);
-    },
-};
-
-
-module.exports = {
-    name: 'poes',
-    description: 'Laat een random poes foto zien',
+    description: 'Laat een mooie poes zien',
     async execute(message, args) {
 
-            const fetch = require('node-fetch');
-            const {body} = await fetch('https://aws.random.cat/meow').then(response => response.json());
+//        const fetch = require('node-fetch');
+        // another option since it is file that comes from the api GET
+        // const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
 
-            message.channel.send(body.file);
+        const response = await fetch('https://aws.random.cat/meow');
+        if (!response.ok) {
+            throw new Error("HTTP status " + response.status);
+        }
+        // note that in body nu de hele JSON zit, als je een specific item wilt showen gebruik body.json_name
+        const body = await response.json();
+
+        message.channel.send(body);
 
     },
 };
